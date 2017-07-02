b420296cb1a602adf6f4dad5eeea38fde7c2cee8
Tcheny <eugenie.be@hotmail.fr>
2017-07-02 20:21:12 +0200

    05 Booleans equal/notEqual done

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,9 +1,9 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  return stringOne === stringTwo ? true : false
 };
 
 module.exports.notEqual = function(one, two) {
-
+  return typeof(one) === typeof(two) ? false : true
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
