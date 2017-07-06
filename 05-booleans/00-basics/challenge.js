3a28ef9c4f642c156f8f9ce8173ed6b7bc96dcef
Tcheny <eugenie.be@hotmail.fr>
2017-07-06 15:53:24 +0200

    05 Bolean Done

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,22 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-  return stringOne === stringTwo ? true : false
+  return stringOne === stringTwo
 };
 
 module.exports.notEqual = function(one, two) {
-  return typeof(one) === typeof(two) ? false : true
+  return one !== two
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-  return lower < middle || middle > upper ? true : false
+  return lower < middle || middle > upper
 };
 
 module.exports.outsideRanges = function(number) {
-
-};
+ var inRange = (10 <= number && number <= 20)
+                || (42 < number && number <= 75)
+                || (1 < number && number < 6);
+                return !inRange
+}
 
 module.exports.nameAndPrice = function(name, price) {
-
+return (name === "NYTimes" || name === "LATimes") && (price >= 1)
 };
